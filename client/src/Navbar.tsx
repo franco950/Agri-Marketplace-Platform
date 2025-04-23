12c9c063fbc1c35f0b241d266974793572906ba1
Danny <127716079+franco950@users.noreply.github.com>
2025-04-23 23:06:56 +0300

    homepage UI

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
-import { useAuth } from "./context/AuthContext";
+import { useAuth } from "./context/useauth";
 
 export default function Navbar() {
   const { isLoggedin, logout } = useAuth();
  
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
+    <nav className="navbar">
+      <div className="logo">AgriMarket</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      
       {isLoggedin ? (
         <>
         <button onClick={logout}>Logout</button>
         <Link to="/profile"><button>profile</button></Link></>
         
       ) : (
-        <Link to="/login"><button>Login</button></Link>
-      )}
+        <li className="sign-in"><Link to="/login">Sign in</Link></li>
+        
+      )}</ul>
     </nav>
   );
 }
--- client/src/homepage.tsx
+++ client/src/homepage.tsx
@@ -1,4 +1,32 @@
+import { Link } from 'react-router-dom';
+
+import Navbar from './Navbar';
 function Homepage(){
-    return (<></>)
+    return (<>
+    <div className='hero'>
+    <div className='glass-card'>
+    <Navbar/>
+    <div className="hero-content">
+        <h1>Direct from Farmers<br/> to You</h1>
+        <p>
+          Source fresh produce and grains directly from local farms.<br />
+          Connecting buyers and farmers for fair, transparent trade.
+        </p>
+        <a href="#" className="explore-btn">Explore Marketplace</a>
+      </div>
+
+      <div className="search-box">
+        <input type="text" placeholder="Search products" />
+        <select>
+        <option  disabled selected>Category</option>
+          
+          <option>Category</option>
+        </select>
+        <select>
+        <option disabled selected>Location</option>
+          <option>Location</option>
+        </select>
+        <button className="search-btn">Search</button>
+      </div></div></div></>)
 }
 export default Homepage
